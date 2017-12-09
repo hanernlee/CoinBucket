bd1029b33f4d58a57799bbc788b41797a34b49c3
Christopher Lee <hanernlee@gmail.com>
2017-12-09 20:14:06 +1100

    First commit

---
+++ Coinster/TestViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TestViewController.swift
+//  Coinster
+//
+//  Created by Christopher Lee on 9/12/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ CoinsterTests/TestViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TestViewController.swift
+//  CoinsterTests
+//
+//  Created by Christopher Lee on 9/12/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+@testable import Coinster
+
+class TestViewControllerSpec: QuickSpec {
+    
+}
