23fac4317be3d2f11bc5c37f8f9db8a6fe8c3edf
Christopher Lee <hanernlee@gmail.com>
2017-12-10 22:40:52 +1100

    Test coin count

---
+++ CoinsterTests/CoinsViewControllerSpec.swift
@@ -0,0 +1,34 @@
+//
+//  CoinsViewControllerSpec.swift
+//  CoinsterTests
+//
+//  Created by Christopher Lee on 10/12/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import Quick
+import Nimble
+
+@testable import Coinster
+
+class CoinsViewControllerSpec: QuickSpec {
+    override func spec() {
+        var subject: CoinsViewController!
+        
+        describe("CoinsViewController") {
+            beforeEach {
+                subject = CoinsViewController(collectionViewLayout: UICollectionViewFlowLayout())
+                _ = subject.collectionView
+            }
+            
+            context("when view is loaded") {
+                it("should have fetched correct amount of coins") {
+                    expect(subject.coins.count).toEventually(equal(10))
+                }
+            }
+        }
+    }
+}
