23b2f8166326ab3b3d73a010cc4a54283037b9b5
Christopher Lee <hanernlee@gmail.com>
2018-01-09 23:45:13 +1100

    TabBar Items

--- CoinBucket/MainTabBarController.swift
+++ CoinBucket/MainTabBarController.swift
@@ -17,6 +17,7 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
 
         setupTabBarItems()
+        tabBar.itemPositioning = .fill
     }
 
     // MARK: - Fileprivate Methods
@@ -25,20 +26,23 @@ class MainTabBarController: UITabBarController {
         
         let coinsViewController = CoinsViewController(collectionViewLayout: UICollectionViewFlowLayout())
         coinsViewController.stateController = stateController
-        let coinsNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "coins_unselected").withRenderingMode(.alwaysOriginal), selectedImage: #imageLiteral(resourceName: "coins_selected").withRenderingMode(.alwaysOriginal), rootViewController: coinsViewController)
+        let coinsNavController = templateNavController(title: "Coins", unselectedImage: #imageLiteral(resourceName: "coins_unselected").withRenderingMode(.alwaysOriginal), selectedImage: #imageLiteral(resourceName: "coins_selected").withRenderingMode(.alwaysOriginal), rootViewController: coinsViewController)
         
         let settingsController = SettingsViewController()
         settingsController.stateController = stateController
-        let settingsNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: settingsController)
+        let settingsNavController = templateNavController(title: "Settings", unselectedImage: #imageLiteral(resourceName: "settings_unselected").withRenderingMode(.alwaysOriginal), selectedImage: #imageLiteral(resourceName: "settings_selected").withRenderingMode(.alwaysOriginal), rootViewController: settingsController)
         
         viewControllers = [coinsNavController, settingsNavController]
     }
     
     // Templating Navigation Controllers
-    fileprivate func templateNavController(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController = UIViewController()) -> UINavigationController {
+    fileprivate func templateNavController(title: String, unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController = UIViewController()) -> UINavigationController {
         let viewController = rootViewController
         let navController = UINavigationController(rootViewController: viewController)
         navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        navController.tabBarItem.titlePositionAdjustment = UIOffsetMake(0, 5)
+        navController.tabBarItem.title = title
         navController.tabBarItem.image = unselectedImage
         navController.tabBarItem.selectedImage = selectedImage
         return navController
