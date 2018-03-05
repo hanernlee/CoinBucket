ac32e0ef9e1f99b08896bcb05b1f22d7fb90ac81
Christopher Lee <hanernlee@gmail.com>
2018-03-05 23:08:51 +1100

    Portfolio view controller

--- CoinBucket/Porfolio/PortfolioViewController.swift
+++ CoinBucket/Porfolio/PortfolioViewController.swift
@@ -11,12 +11,25 @@ import UIKit
 class PortfolioViewController: UIViewController {
     
     var stateController: StateController!
+    
+    let emptyView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .groupTableViewBackground
         navigationItem.title = "Portfolio"
+        
+        configureUI()
 
+    }
+    
+    fileprivate func configureUI() {
+        view.addSubview(emptyView)
+        emptyView.frame = CGRect(x: 0, y: (navigationController?.navigationBar.frame.height)! + 40, width: view.frame.width, height: view.frame.height / 2 - 50)
     }
 }
