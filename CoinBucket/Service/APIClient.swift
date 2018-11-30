a46e67a0b82dd8c82465cccb7d738c4cf3200316
Christopher Lee <hanernlee@gmail.com>
2018-12-01 00:11:55 +1100

    WIP

--- CoinBucket/Service/APIClient.swift
+++ CoinBucket/Service/APIClient.swift
@@ -13,18 +13,18 @@ public struct APIClient {
     static let baseUrl: String = "https://cryptocompare.com"
 
     static func getCoins(page: Int = 0, currency: String) -> String {
-        return "\(apiUrl)/data/top/totalvol?limit=50&page=\(page)&tsym=\(currency)&api_key=1875293a1a09a4cf9d9a5e6d276ca9a718b0f9d7c58a17fc3b75519fb360122e"
+        return "\(apiUrl)/data/top/totalvol?limit=50&page=\(page)&tsym=\(currency)"
     }
     
     static func getPrices(_ symbols: String, currency: String) -> String {
-        return "\(apiUrl)/data/pricemultifull?fsyms=\(symbols)&tsyms=\(currency),BTC&api_key=1875293a1a09a4cf9d9a5e6d276ca9a718b0f9d7c58a17fc3b75519fb360122e"
+        return "\(apiUrl)/data/pricemultifull?fsyms=\(symbols)&tsyms=\(currency),BTC"
     }
     
     static func getSuggestions(_ searchText: String) -> String {
-        return "\(baseUrl)/api/autosuggest/all/?maxRows=10&q=\(searchText)&api_key=1875293a1a09a4cf9d9a5e6d276ca9a718b0f9d7c58a17fc3b75519fb360122e"
+        return "\(baseUrl)/api/autosuggest/all/?maxRows=10&q=\(searchText)"
     }
     
     static func getCoin(symbols: String, currency: String) -> String {
-        return "\(apiUrl)/data/coin/generalinfo?fsyms=\(symbols)&tsym=\(currency)&api_key=1875293a1a09a4cf9d9a5e6d276ca9a718b0f9d7c58a17fc3b75519fb360122e"
+        return "\(apiUrl)/data/coin/generalinfo?fsyms=\(symbols)&tsym=\(currency)"
     }
 }
